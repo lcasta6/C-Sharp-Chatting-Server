223f42439357353588e4034ea72963d3cd4e582c
lcasta6 <lcasta6@uic.edu>
2020-05-10 19:46:10 -0500

    Finished

---
+++ Client/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Set up an ipAdress to local host and then
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint clientConnection = new IPEndPoint(ipAddress,5555);
+            Socket clientSocket = new Socket(SocketType.Stream,ProtocolType.Tcp);
+            
+            //Try to connect to the remote device and throw exceptions if necessary
+            //Besides ArgumentNull And SocketException all will be handled by the last catch
+            try
+            {
+                clientSocket.Connect(clientConnection);
+                Console.WriteLine("Connection Established");
+            }catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNull: {0}", e.Message);
+            }catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e.Message);
+            }catch (Exception e)
+            {
+                Console.WriteLine("Some other exception occured: ");
+                Console.WriteLine($"{e.GetType()} was thrown message {e.Message}");
+            }
+            
+            //At this point the client should be connected so they should be able to receive and
+            //Transmit messages to the server.
+            Thread receiveMessages = new Thread(()=>ReceiveMessages(clientSocket));           
+            receiveMessages.Start();
+            
+            //For the rest of this code we'll set up the reception back
+            byte[] serverMessages = new byte[10025];
+            while (true)
+            {
+                clientSocket.Receive(serverMessages);
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(serverMessages));
+                Array.Clear(serverMessages,0,serverMessages.Length);
+            }
+        }//End of Main
+
+        private static void ReceiveMessages(Socket clientSocket)
+        {
+            //Sending Messages to the server
+            while (true)
+            {
+                string stringMessage = Console.ReadLine();
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(stringMessage);
+                clientSocket.Send(msg);
+                Thread.Sleep(200);
+            }
+        }
+        
+    }
+}
---
+++ Server/ConsoleApp2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //Will be used to transmit messages out to the clients
+        private static List<Socket> clients = new List<Socket>();
+        
+        static void Main(string[] args)
+        {
+            //Keeps count of the clients
+            int count = 0;
+            
+            /*We get the information fo the server and set up a listener for it*/
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint serverConnection = new IPEndPoint(ipAddress,5555);
+            
+            Socket listener = new Socket(SocketType.Stream,ProtocolType.Tcp);
+
+            /*This is where the client and server diverge in their implementation,
+             this is the point where the listener is binned to a port and is listening*/
+            try
+            {
+                listener.Bind(serverConnection);
+                listener.Listen(5);
+            }catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Null Connection: {0}", e.ToString());
+
+            }catch (SocketException e)
+            {
+                Console.WriteLine("Socket could not be accessed: {0}", e.ToString());
+            }catch (Exception e)
+            {
+                Console.WriteLine("Unspecified Exceptions: {0}", e.ToString());
+            }
+
+            //We wait for our first client to come in
+            //firstClient Socket will be associated with that one client
+            Console.WriteLine("Waiting for the client to come in");
+            while (true)
+            {
+                //Get the next client
+                Socket inClient = listener.Accept();
+                clients.Add(inClient);
+                
+                //Start a new Thread for this Client
+                Thread newClient = new Thread(()=>HandleClients(inClient, count));
+                newClient.Start();
+                count++;
+            }
+            
+            
+            
+        }//End of Main
+
+        private static void HandleClients(Socket inClient, int count)
+        {
+            //After we get a client we can begin our execution
+            Console.WriteLine("New client connected");
+            byte[] clientMessage = new byte[10025];
+            while (true)
+            {
+                try
+                {
+                    //Receive from the Client
+                    inClient.Receive(clientMessage);
+                    
+                    //If the client has disconnected then we just end the program
+                    if (clientMessage[0] == default(byte))
+                    {
+                        throw new SystemException();
+                    }
+                    
+                    Console.Write($"Client #{count} said: ");
+                    Console.WriteLine(System.Text.Encoding.ASCII.GetString(clientMessage) + "\n");
+                    MessageToClients(count,System.Text.Encoding.ASCII.GetString(clientMessage));
+                    Array.Clear(clientMessage,0,clientMessage.Length);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong, closed Socket");
+                    clients.Remove(inClient);
+                    break;
+                }
+            }//End of While(true) loop
+        }//End of HandleClients
+
+        private static void MessageToClients(int clientNumber,string message)
+        {
+            foreach (var i in clients)
+            {
+                i.Send(System.Text.Encoding.ASCII.GetBytes($"Client#{clientNumber} said {message}"));
+            }
+        }
+        
+
+        
+    }
+}
